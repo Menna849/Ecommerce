f2ee6fb5e5c242a5b8359647e77378e2c17b1c02
Mena Ghanem <menayasser849@std.mans.edu.eg>
2022-04-28 06:29:29 -0700

    upload files

---
+++ Mobile_Shopee/index.php
@@ -0,0 +1,40 @@
+<?php
+  // include header.php file
+  include('header.php');
+?>
+
+<?php
+     /*  include banner area  */
+      include('Template/_banner-area.php');
+     /*  include banner area  */
+
+    /*  include top sale section  */
+     include('Template/_tob-sale.php');
+   /*  include top sale section */
+
+
+   /*  include special price section*/
+    include('Template/_special-price.php');
+   /*  include special price section */
+
+
+   /*  include banner ads  */
+    include('Template/_special-price.php');
+  /*  include banner ads  */
+
+   /*  include new phone section*/
+    include('Template/_new-phones.php');
+   /*  include new phone section*/
+
+  /*  include blog area */
+   include ('Template/_new-phones.php');
+  /*  include blog area  */
+
+?>
+
+<?php
+// include footer.php file
+include('footer.php');
+?>
+
+
